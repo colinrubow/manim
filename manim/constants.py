fabae47d6596ea5ae1b406b2607636db1f4ccde3
Colin Rubow <colinwrubow@gmail.com>
2026-07-08 21:21:33 -0600

    added pyramid tik shape

--- manim/constants.py
+++ manim/constants.py
@@ -79,6 +79,8 @@ __all__ = [
     "TIK_CIRCLE",
     "TIK_CYLINDER",
     "TIK_CUBE",
+    "TIK_SQUARE",
+    "TIK_PYRAMID",
 ]
 
 # Tikz shapes
@@ -86,6 +88,8 @@ TIK_CONE = r'\tikz[baseline,scale=0.2]{\draw(-1,0) -- (0, 1.55) -- (1,0); \draw(
 TIK_CIRCLE = r'\tikz[baseline,scale=0.2]{\draw(0,0.4) circle (1)}'
 TIK_CYLINDER = r'\tikz[baseline,scale=0.2]{\draw(-1,-0.15) -- (-1, 1.4); \draw(0, 1.4) ellipse (1 and 0.35); \draw(1, 1.4) -- (1, -.15); \draw(-1,-0.15) arc (-180:0:1 and 0.35);}'
 TIK_CUBE = r'\tikz[baseline,scale=0.2]{\draw(-1,0) -- (-1, 1.05) -- (0, 1.4) -- (1, 1.05) -- (1, 0) -- (0, -0.35) -- (-1, 0);\draw(-1, 1.05) -- (0, 0.7) -- (1, 1.05);\draw(0, 0.7) -- (0, -0.35);}'
+TIK_SQUARE = r'\tikz[baseline,scale=0.2]{\draw(-1, -0.5) -- (-1, 1.5) -- (1, 1.5) -- (1, -0.5) -- cycle}'
+TIK_PYRAMID = r'\tikz[baseline,scale=0.2]{\draw(-1,0) -- (0, 1.55) -- (1,0) -- (0, -0.3) -- cycle; \draw(0, -0.3) -- (0, 1.55);}'
 
 # Messages
 
