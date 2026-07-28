ff3dcb90c402195c759dd49c723f37991a5d4770
Colin Rubow <colinwrubow@gmail.com>
2026-07-28 08:43:53 -0600

    new tik constant

--- manim/constants.py
+++ manim/constants.py
@@ -85,6 +85,7 @@ __all__ = [
     "TIK_OCTAGON",
     "TIK_RADIUS",
     "TIK_CONE_AXIS",
+    "TIK_CYL_AXIS",
 ]
 
 # Tikz shapes
@@ -94,6 +95,7 @@ TIK_CIRCLE = r'\tikz[baseline,scale=0.2]{\draw(0,0.4) circle (1)}'
 TIK_DIAMETER = r'\tikz[baseline,scale=0.2]{\draw(0,0.4) circle (1); \draw(-1,0.4) -- (1,0.4);}'
 TIK_RADIUS = r'\tikz[baseline,scale=0.2]{\draw(0,0.4) circle (1); \draw(-1,0.4) -- (0,0.4);}'
 TIK_CYLINDER = r'\tikz[baseline,scale=0.2]{\draw(-1,-0.15) -- (-1, 1.4); \draw(0, 1.4) ellipse (1 and 0.35); \draw(1, 1.4) -- (1, -.15); \draw(-1,-0.15) arc (-180:0:1 and 0.35);}'
+TIK_CYL_AXIS = r'\tikz[baseline,scale=0.2]{\draw(-1,-0.15) -- (-1, 1.4); \draw(0, 1.4) ellipse (1 and 0.35); \draw(1, 1.4) -- (1, -.15); \draw(-1,-0.15) arc (-180:0:1 and 0.35); \draw(0, 1.5) -- (0, 0);}'
 TIK_CUBE = r'\tikz[baseline,scale=0.2]{\draw(-1,0) -- (-1, 1.05) -- (0, 1.4) -- (1, 1.05) -- (1, 0) -- (0, -0.35) -- (-1, 0);\draw(-1, 1.05) -- (0, 0.7) -- (1, 1.05);\draw(0, 0.7) -- (0, -0.35);}'
 TIK_SQUARE = r'\tikz[baseline,scale=0.2]{\draw(-1, -0.5) -- (-1, 1.5) -- (1, 1.5) -- (1, -0.5) -- cycle}'
 TIK_PYRAMID = r'\tikz[baseline,scale=0.2]{\draw(-1,0) -- (0, 1.55) -- (1,0) -- (0, -0.3) -- cycle; \draw(0, -0.3) -- (0, 1.55);}'
